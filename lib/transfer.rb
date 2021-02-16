edebac2d46d4be7ff04790785e087320e0c03290
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-16 17:09:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Transfer
   attr_accessor :sender, :receiver, :status, :amount
 
-  def initialize
+  def initialize(sender, receiver, amount = 50)
     @sender = sender
     @receiver = receiver
     @status = 'pending'
