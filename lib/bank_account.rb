42ac37d4c8b1b5a91bbeb39227458f28d6e3778c
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-16 16:34:31 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,9 +3,9 @@ class BankAccount
   attr_accessor :status, :balance
   attr_reader :name
 
-  def initialize(name)
+  def initialize(name, balance = 1000, status = open)
     @name = name
     @balance = 1000
-    @status = open
+    @status = status
   end
 end
